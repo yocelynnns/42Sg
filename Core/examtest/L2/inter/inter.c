9017ed6d9ebdba63be53d1fb3316c302bd0e78f7
yocelynnns <ytheonas05@gmail.com>
2024-08-12 20:08:34 +0800

    sad

--- Core/examtest/L2/inter/inter.c
+++ Core/examtest/L2/inter/inter.c
@@ -2,23 +2,24 @@
 
 int main(int argc, char **argv)
 {
-	int i = 0;
-	int j = 0;
-	int seen[256] = {0};
+	int i;
+	int j;
+	char seen[256] = {0};
+
 	if (argc == 3)
 	{
+		i = 0;
 		while (argv[1][i])
 		{
+			j = 0;
 			while (argv[2][j])
 			{
 				if (argv[1][i] == argv[2][j])
 				{
-					if (!seen[(unsigned char)argv[1][i]])
+					if(!seen[(unsigned char)argv[1][i]])
 					{
-						write(1, &argv[1][i], 1);
-						seen[(unsigned
-								char)argv[1][i]]
-							= 1;
+						write (1, &argv[1][i], 1);
+						seen[(unsigned char)argv[1][i]] = 1;
 					}
 					break;
 				}
@@ -28,5 +29,4 @@ int main(int argc, char **argv)
 		}
 	}
 	write (1, "\n", 1);
-	return (0);
 }
