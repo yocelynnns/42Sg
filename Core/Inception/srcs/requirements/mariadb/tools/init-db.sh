08785dbff4565ac97dc356bb455daab550def923
yset <ytheonas05@gmail.com>
2025-08-21 22:14:16 +0800

    incep

--- Core/Inception/srcs/requirements/mariadb/tools/init-db.sh
+++ Core/Inception/srcs/requirements/mariadb/tools/init-db.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-set -e
+service mysql start
 
-if [ ! -d "/var/lib/mysql/mysql" ]; then
-    mariadbd --initialize-insecure --user=mysql
-fi
-
-mariadbd --skip-networking &
-sleep 5
+# Read secrets
+MYSQL_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
+MYSQL_PASSWORD=$(cat /run/secrets/db_password)
 
 mysql -u root <<EOF
 CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
@@ -15,5 +12,5 @@ GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
 
-mysqladmin -u root shutdown
-exec mariadbd
+# Prevent container from exiting
+exec mysqld_safe
